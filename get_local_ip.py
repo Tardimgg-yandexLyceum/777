dd9d05a67c8e0a154424b8e24113ed88ff2dfe38
alekela <76111160+alekela@users.noreply.github.com>
2021-04-06 10:20:46 +0300

    Update get_local_ip.py

--- get_local_ip.py
+++ get_local_ip.py
@@ -7,7 +7,7 @@ def get_ip():
     if platform == 'linux' or platform == "linux2":
         res = subprocess.check_output("hostname -I", shell=True).split()[0]
         return "".join(list(map(lambda x: chr(x), res)))
-    if platform == 'win32':
+    if platform == 'win32' or platform == 'win64':
 
         hostname = socket.gethostname() 
         IP = socket.gethostbyname(hostname) 
